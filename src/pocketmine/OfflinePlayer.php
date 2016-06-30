d16b17792ee05aba249cc776d9414b6ccd6d5136
Professor Taki <Taki21@users.noreply.github.com>
2016-06-30 10:11:32 -0400

    Update OfflinePlayer.php

--- src/pocketmine/OfflinePlayer.php
+++ src/pocketmine/OfflinePlayer.php
@@ -1,34 +1,4 @@
 <?php
-/**
- * src/pocketmine/OfflinePlayer.php
- *
- * @package default
- */
-
-
-/*
- *
- *  _                       _           _ __  __ _
- * (_)                     (_)         | |  \/  (_)
- *  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___
- * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \
- * | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/
- * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___|
- *                     __/ |
- *                    |___/
- *
- * This program is a third party build by ImagicalMine.
- *
- * PocketMine is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author ImagicalMine Team
- * @link http://forums.imagicalmine.net/
- *
- *
-*/
 
 namespace pocketmine;
 
@@ -43,11 +13,6 @@ class OfflinePlayer implements IPlayer
     private $server;
     private $namedtag;
 
-    /**
-     *
-     * @param Server  $server
-     * @param string  $name
-     */
     public function __construct(Server $server, $name)
     {
         $this->server = $server;
@@ -59,51 +24,26 @@ class OfflinePlayer implements IPlayer
         }
     }
 
-
-    /**
-     *
-     * @return unknown
-     */
     public function isOnline()
     {
         return $this->getPlayer() !== null;
     }
 
-
-    /**
-     *
-     * @return unknown
-     */
     public function getName()
     {
         return $this->name;
     }
 
-
-    /**
-     *
-     * @return unknown
-     */
     public function getServer()
     {
         return $this->server;
     }
 
-
-    /**
-     *
-     * @return unknown
-     */
     public function isOp()
     {
         return $this->server->isOp(strtolower($this->getName()));
     }
 
-
-    /**
-     *
-     * @param unknown $value
-     */
     public function setOp($value)
     {
         if ($value === $this->isOp()) {
@@ -117,21 +57,11 @@ class OfflinePlayer implements IPlayer
         }
     }
 
-
-    /**
-     *
-     * @return unknown
-     */
     public function isBanned()
     {
         return $this->server->getNameBans()->isBanned(strtolower($this->getName()));
     }
 
-
-    /**
-     *
-     * @param unknown $value
-     */
     public function setBanned($value)
     {
         if ($value === true) {
@@ -142,20 +72,11 @@ class OfflinePlayer implements IPlayer
     }
 
 
-    /**
-     *
-     * @return unknown
-     */
     public function isWhitelisted()
     {
         return $this->server->isWhitelisted(strtolower($this->getName()));
     }
 
-
-    /**
-     *
-     * @param unknown $value
-     */
     public function setWhitelisted($value)
     {
         if ($value === true) {
@@ -165,85 +86,42 @@ class OfflinePlayer implements IPlayer
         }
     }
 
-
-    /**
-     *
-     * @return unknown
-     */
     public function getPlayer()
     {
         return $this->server->getPlayerExact($this->getName());
     }
 
-
-    /**
-     *
-     * @return unknown
-     */
     public function getFirstPlayed()
     {
         return $this->namedtag instanceof CompoundTag ? $this->namedtag["firstPlayed"] : null;
     }
 
-
-    /**
-     *
-     * @return unknown
-     */
     public function getLastPlayed()
     {
         return $this->namedtag instanceof CompoundTag ? $this->namedtag["lastPlayed"] : null;
     }
 
-
-    /**
-     *
-     * @return unknown
-     */
     public function hasPlayedBefore()
     {
         return $this->namedtag instanceof CompoundTag;
     }
 
-
-    /**
-     *
-     * @param unknown       $metadataKey
-     * @param MetadataValue $metadataValue
-     */
     public function setMetadata($metadataKey, MetadataValue $metadataValue)
     {
         $this->server->getPlayerMetadata()->setMetadata($this, $metadataKey, $metadataValue);
     }
 
-
-    /**
-     *
-     * @param unknown $metadataKey
-     * @return unknown
-     */
     public function getMetadata($metadataKey)
     {
         return $this->server->getPlayerMetadata()->getMetadata($this, $metadataKey);
     }
 
-
-    /**
-     *
-     * @param unknown $metadataKey
-     * @return unknown
-     */
     public function hasMetadata($metadataKey)
     {
         return $this->server->getPlayerMetadata()->hasMetadata($this, $metadataKey);
     }
 
 
-    /**
-     *
-     * @param unknown $metadataKey
-     * @param Plugin  $plugin
-     */
     public function removeMetadata($metadataKey, Plugin $plugin)
     {
         $this->server->getPlayerMetadata()->removeMetadata($this, $metadataKey, $plugin);
