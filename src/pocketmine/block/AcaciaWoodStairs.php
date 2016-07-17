a9e2bcda2042adbf35bce708fcf26d181e3ef060
remote_vase <web.articlerequests@gmail.com>
2016-07-17 08:49:26 -0400

    Continue SMOOTH-REWRITE in the master branch

--- src/pocketmine/block/AcaciaWoodStairs.php
+++ src/pocketmine/block/AcaciaWoodStairs.php
@@ -1,34 +1,5 @@
 <?php
-/**
- * src/pocketmine/block/AcaciaWoodStairs.php
- *
- * @package default
- */
 
-
-/*
- *
- *  _                       _           _ __  __ _
- * (_)                     (_)         | |  \/  (_)
- *  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___
- * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \
- * | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/
- * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___|
- *                     __/ |
- *                    |___/
- *
- * This program is a third party build by ImagicalMine.
- *
- * PocketMine is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author ImagicalMine Team
- * @link http://forums.imagicalcorp.ml/
- *
- *
-*/
 
 namespace pocketmine\block;
 
@@ -40,31 +11,16 @@ class AcaciaWoodStairs extends Stair
 
     protected $id = self::ACACIA_WOOD_STAIRS;
 
-    /**
-     *
-     * @param unknown $meta (optional)
-     */
     public function __construct($meta = 0)
     {
         $this->meta = $meta;
     }
 
-
-    /**
-     *
-     * @return unknown
-     */
     public function getName()
     {
         return "Acacia Wood Stairs";
     }
 
-
-    /**
-     *
-     * @param Item    $item
-     * @return unknown
-     */
     public function getDrops(Item $item)
     {
         return [
@@ -72,31 +28,16 @@ class AcaciaWoodStairs extends Stair
         ];
     }
 
-
-    /**
-     *
-     * @return unknown
-     */
     public function getHardness()
     {
         return 2;
     }
 
-
-    /**
-     *
-     * @return unknown
-     */
     public function getResistance()
     {
         return 15;
     }
 
-
-    /**
-     *
-     * @return unknown
-     */
     public function getToolType()
     {
         return Tool::TYPE_AXE;
