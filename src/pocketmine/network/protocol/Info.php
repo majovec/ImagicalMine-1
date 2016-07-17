7128d3dab332958ae82c74d2170636e393a5482a
DarkWav <simon.vogl@gmx.net>
2016-07-17 10:58:51 +0200

    Revert "Try to support 0.15.1"
    This reverts commit 7b46e3d1a4ac6815a66ab8f1e0ac303e57f2382b.

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -36,7 +36,7 @@ interface Info
      * Actual Minecraft: PE protocol version
      */
     const CURRENT_PROTOCOL = 81;
-    const ACCEPTED_PROTOCOLS = [81,82,83,84,85,86,87,88,89,90];
+    const ACCEPTED_PROTOCOLS = [81];
     const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
 	const SERVER_TO_CLIENT_HANDSHAKE_PACKET = 0x03;
