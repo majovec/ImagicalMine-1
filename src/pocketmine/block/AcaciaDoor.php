36b5ab2bc8400bfa4f1fd0b9d0c9e209f1745c5f
remote_vase <web.articlerequests@gmail.com>
2016-07-17 08:48:48 -0400

    Start SMOOTH-REWRITE in the master branch

--- src/pocketmine/block/AcaciaDoor.php
+++ src/pocketmine/block/AcaciaDoor.php
@@ -1,34 +1,5 @@
 <?php
-/**
- * src/pocketmine/block/AcaciaDoor.php
- *
- * @package default
- */
 
-
-/*
- *
- *  _                       _           _ __  __ _
- * (_)                     (_)         | |  \/  (_)
- *  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___
- * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \
- * | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/
- * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___|
- *                     __/ |
- *                    |___/
- *
- * This program is a third party build by ImagicalMine.
- *
- * PocketMine is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author ImagicalMine Team
- * @link http://forums.imagicalcorp.ml/
- *
- *
-*/
 
 namespace pocketmine\block;
 
@@ -40,61 +11,33 @@ class AcaciaDoor extends Door
 
     protected $id = self::ACACIA_DOOR_BLOCK;
 
-    /**
-     *
-     * @param unknown $meta (optional)
-     */
+    
     public function __construct($meta = 0)
     {
         $this->meta = $meta;
     }
 
 
-    /**
-     *
-     * @return unknown
-     */
+ 
     public function getName()
     {
         return "Acacia Door Block";
     }
 
 
-    /**
-     *
-     * @return unknown
-     */
     public function canBeActivated()
     {
         return true;
     }
 
-
-    /**
-     *
-     * @return unknown
-     */
     public function getHardness()
     {
         return 3;
     }
-
-
-    /**
-     *
-     * @return unknown
-     */
     public function getToolType()
     {
         return Tool::TYPE_AXE;
     }
-
-
-    /**
-     *
-     * @param Item    $item
-     * @return unknown
-     */
     public function getDrops(Item $item)
     {
         return [
