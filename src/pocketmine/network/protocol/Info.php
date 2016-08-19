680e4358182144fc60abf0ea0701192e4c34f8d3
remote_vase <web.articlerequests@gmail.com>
2016-08-19 15:00:48 -0400

    Finish 0.15 update

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -1,39 +1,13 @@
 <?php
 
-/*
- *
- *  _                       _           _ __  __ _
- * (_)                     (_)         | |  \/  (_)
- *  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___
- * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \
- * | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/
- * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___|
- *                     __/ |
- *                    |___/
- *
- * This program is a third party build by ImagicalMine.
- *
- * PocketMine is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author ImagicalMine Team
- * @link http://forums.imagicalcorp.ml/
- *
- *
-*/
 
-/**
- * Minecraft: PE multiplayer protocol implementation
- */
 namespace pocketmine\network\protocol;
 interface Info{
 	/**
 	 * Actual Minecraft: PE protocol version
 	 */
 	const CURRENT_PROTOCOL = 82;
-	const ACCEPTED_PROTOCOLS = [81, 82];
+	const ACCEPTED_PROTOCOLS = [81, 82, 83];
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
 	const SERVER_TO_CLIENT_HANDSHAKE_PACKET = 0x03;
